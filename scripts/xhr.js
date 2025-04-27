472bda395d83f5a7b5d0694b359bca95603735fc
zhongzhaoli <525966315@qq.com>
2025-04-27 16:37:03 +0800

    feat: 时间回调

--- scripts/xhr.js
+++ scripts/xhr.js
@@ -108,15 +108,15 @@ function fetchCore(formData, url) {
 }
 
 function generateMonthRange() {
-  // // 获取当前日期
-  // const today = new Date();
-  // const endDate = new Date(today);
+  // 获取当前日期
+  const today = new Date();
+  const endDate = new Date(today);
 
-  // // 计算7天前的日期
-  // const startDate = new Date(today);
-  // startDate.setDate(today.getDate() - 6); // 减6得到7天（包括今天）
-  const startDate = new Date('2025-01-31');
-  const endDate = new Date('2025-02-01');
+  // 计算7天前的日期
+  const startDate = new Date(today);
+  startDate.setDate(today.getDate() - 6); // 减6得到7天（包括今天）
+  // const startDate = new Date('2025-01-31');
+  // const endDate = new Date('2025-02-01');
 
   const result = [];
   const currentDate = new Date(startDate);
